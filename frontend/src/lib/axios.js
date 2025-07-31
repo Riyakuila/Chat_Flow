3e0ab14c2160a69f5a17e72648c122cc28709feb
Debjyoti Shit <debyotishit27@gmail.com>
2025-07-31 22:40:50 +0530

    Fixed all the bug

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.VITE_BACKEND_URL}/api`,
+  baseURL: `${process.env.VITE_BACKEND_URL}api`,
   withCredentials: true,
 })
 
